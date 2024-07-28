4a21558d087d9c64878baec1cb30c8ecb846ff1f
luiz.morais <luiz.morais@uhgbrasil.com.br>
2024-07-28 17:16:55 -0300

    criação da tabela no docker compose

--- InitializeDB.sql
+++ InitializeDB.sql
@@ -1,4 +1,6 @@
-create table contato (
+create database if not exists contatos;
+use contatos;
+create table if not exists contato (
     id int auto_increment primary key,
     nome varchar(255) not null,
     telefone varchar(15) not null,
