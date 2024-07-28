3ff2df60e1c7f612252a0fbc97dc76fce8f46c32
wduty <wduty@hotmail.com>
2024-07-28 16:20:37 -0300

    Correção sql de criação de tabelas

--- criação tabelas.sql
+++ criação tabelas.sql
@@ -2,5 +2,5 @@ create table contato (
     id int auto_increment primary key,
     nome varchar(255) not null,
     telefone varchar(15) not null,
-    email varchar(255) not null,
+    email varchar(255) not null
 );
