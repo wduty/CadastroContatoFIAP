90090ac988a22580bd2f76aaa14f572891fc6cce
Luiz Faraco <wduty@hotmail.com>
2024-07-28 15:50:42 -0300

    Adição de testes unitarios

--- CadastroContatosInfrastructure/Repository/RepositorioRegiaoDDD.cs
+++ CadastroContatosInfrastructure/Repository/RepositorioRegiaoDDD.cs
@@ -1,23 +1,27 @@
 ﻿using CadastroContatosDomain.Entidades;
 using CadastroContatosDomain.Interfaces.Repositorio;
+using System.IO;
 
-namespace CadastroContatosInfrastructure.Repository;
-
-public class RepositorioRegiaoDDD : IRepositorioRegiaoDDD
+namespace CadastroContatosInfrastructure.Repository
 {
-    List<RegiaoDDD> RegioesDDD = new();
-
-    public RepositorioRegiaoDDD()
+    public class RepositorioRegiaoDDD : IRepositorioRegiaoDDD
     {
-        foreach (var linha in File.ReadAllLines("RegioesDDD.txt"))
+        List<RegiaoDDD> RegioesDDD = new();
+
+        public RepositorioRegiaoDDD()
         {
-            var campos = linha.Split(';');
-            if (campos.Length != 2)
-                throw new Exception($"Registro de região de DDD inválida: {linha}");
+            foreach (var linha in ObterLinhasDoArquivo())
+            {
+                var campos = linha.Split(';');
+                if (campos.Length != 2)
+                    throw new Exception($"Registro de região de DDD inválida: {linha}");
 
-            RegioesDDD.Add(new RegiaoDDD() { Regiao = campos[0], DDD = campos[1] });
+                RegioesDDD.Add(new RegiaoDDD() { Regiao = campos[0], DDD = campos[1] });
+            }
         }
-    }
 
-    public RegiaoDDD? BuscarPorDDD(string ddd) => RegioesDDD.FirstOrDefault(r => r.DDD == ddd);
+        public virtual string[] ObterLinhasDoArquivo() => File.ReadAllLines("RegioesDDD.txt");
+
+        public RegiaoDDD? BuscarPorDDD(string ddd) => RegioesDDD.FirstOrDefault(r => r.DDD == ddd);
+    }
 }
---
+++ CadastroContatosTestesUnitarios/Application/TesteServiceContatos.cs
@@ -0,0 +1,96 @@
+﻿using CadastroContatosApplication.Service;
+using CadastroContatosDomain.Entidades;
+using CadastroContatosDomain.Interfaces.Repositorio;
+using Moq;
+
+namespace CadastroContatosTestesUnitarios.Application
+{
+    [TestFixture]
+    public class ServiceContatosTests
+    {
+        private Mock<IServiceProvider> mockServiceProvider;
+        private Mock<IUnitOfWork> mockUnitOfWork;
+        private Mock<IRepositorioContato> mockRepositorioContato;
+        private ServiceContatos serviceContatos;
+
+        [SetUp]
+        public void Setup()
+        {
+            mockServiceProvider = new Mock<IServiceProvider>();
+            mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockRepositorioContato = new Mock<IRepositorioContato>();
+
+            mockUnitOfWork.Setup(u => u.RepositorioContato).Returns(mockRepositorioContato.Object);
+
+            mockServiceProvider.Setup(sp => sp.GetService(typeof(IUnitOfWork))).Returns(mockUnitOfWork.Object);
+
+            serviceContatos = new ServiceContatos(mockServiceProvider.Object);
+        }
+
+        [Test]
+        public async Task CadastrarContato_DeveChamarRepositorioCadastrarContato()
+        {
+            // Arrange
+            var contato = new ContatoPessoa { Nome = "Teste", Telefone = "11 91234-5678", EMail = "teste@teste.com" };
+
+            // Act
+            await serviceContatos.CadastrarContato(contato);
+
+            // Assert
+            mockRepositorioContato.Verify(r => r.CadastrarContato(contato), Times.Once);
+            mockUnitOfWork.Verify(u => u.BeginTransaction(), Times.Once);
+            mockUnitOfWork.Verify(u => u.Commit(), Times.Once);
+        }
+
+        [Test]
+        public async Task BuscarPorDDD_DeveRetornarContatos()
+        {
+            // Arrange
+            var ddd = "11";
+            var contatosMock = new List<Contato>
+            {
+                new Contato { Nome = "Teste", Telefone = "11 91234-5678", EMail = "teste@teste.com" }
+            };
+
+            mockRepositorioContato.Setup(r => r.BuscaPorDDD(ddd)).ReturnsAsync(contatosMock);
+
+            // Act
+            var result = await serviceContatos.BuscarPorDDD(ddd);
+
+            // Assert
+            Assert.That(result.Count(), Is.EqualTo(1));
+            Assert.That(result.First().Telefone.Substring(0, 2), Is.EqualTo("11"));
+            mockRepositorioContato.Verify(r => r.BuscaPorDDD(ddd), Times.Once);
+        }
+
+        [Test]
+        public async Task AlterarContato_DeveChamarRepositorioAlterarContato()
+        {
+            // Arrange
+            var contato = new ContatoPessoa { Nome = "Teste", Telefone = "11 91234-5678", EMail = "teste@teste.com" };
+
+            // Act
+            await serviceContatos.AlterarContato(contato);
+
+            // Assert
+            mockRepositorioContato.Verify(r => r.AlterarContato(contato), Times.Once);
+            mockUnitOfWork.Verify(u => u.BeginTransaction(), Times.Once);
+            mockUnitOfWork.Verify(u => u.Commit(), Times.Once);
+        }
+
+        [Test]
+        public async Task ExcluirContato_DeveChamarRepositorioExcluirContato()
+        {
+            // Arrange
+            var nome = "Teste";
+
+            // Act
+            await serviceContatos.ExcluirContato(nome);
+
+            // Assert
+            mockRepositorioContato.Verify(r => r.ExcluirContato(nome), Times.Once);
+            mockUnitOfWork.Verify(u => u.BeginTransaction(), Times.Once);
+            mockUnitOfWork.Verify(u => u.Commit(), Times.Once);
+        }
+    }
+}
